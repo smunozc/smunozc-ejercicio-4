22524f2e1bb90d4a628f0dd54792d61191f843eb
smunozc <salvador.munoz0109@gmail.com>
2020-11-11 14:39:28 +0100

    1 file modified

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ const changeBackgroundButton = document.getElementById(
   "changeBackgroundButton"
 );
 
-changeBackgroundButton.addEventListener("click", function() {
-  this.style.backgroundColor = getRandomColor();
+changeBackgroundButton.addEventListener("click", () => {
+  changeBackgroundButton.style.backgroundColor = getRandomColor();
 });
 
 function getRandomColor() {
   const letters = getLetters();
-  let color = '#';
+  let color = "#";
   for (var i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
 }
 
-function getLetters(){
-  return '0123456789ABCDEF';
+function getLetters() {
+  return "0123456789ABCDEF";
 }
